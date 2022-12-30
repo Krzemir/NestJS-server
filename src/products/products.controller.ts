70fecb8156a97bba64b096393fa9b3256bc09276
Krzysztof Mirończuk <krzysztof.mironczuk@socart.media>
2022-12-30 09:56:32 +0100

    nestJS server checkpoint

---
+++ src/products/products.controller.ts
@@ -0,0 +1,31 @@
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { CreateProductDTO } from './dtos/create-product.dto';
+import { ProductsService } from './products.service';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private productsService: ProductsService) {
+    this.productsService = productsService;
+  }
+
+  @Get('/')
+  public getAny(): any {
+    return this.productsService.getAll();
+  }
+
+  @Get('/:id')
+  public getById(@Param('id') id: string) {
+    return this.productsService.getById(id);
+  }
+
+  @Delete('/:id')
+  public removeById(@Param('id') id: string) {
+    this.productsService.removeById(id);
+    return { success: true };
+  }
+
+  @Post('/')
+  create(@Body() productData: CreateProductDTO) {
+    return this.productsService.create(productData);
+  }
+}
